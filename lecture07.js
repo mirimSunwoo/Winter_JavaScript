bb130a4336d315e0702480650af328c43e42695e
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-20 14:35:00 +0900

    Object

---
+++ lecture07.js
@@ -0,0 +1,14 @@
+//객체 만들기
+//객체 : 속성(property)의 집합
+//변수는 복잡한 값을 관리하기 편함
+var empty_ogj = {};
+typeof(empty_ogj);
+
+var man = { name :"홍길동",age:20,height:180};
+
+// man.name - man객체의 name출력- 1
+// man.["name"] - man객체의 name출력-2
+
+// man.name="이몽룡" - 객체 이름.속성이름으로 접근하기
+// man["age"]=15 - age의 값을 15로 바꾸기
+// man - 전부 출력
