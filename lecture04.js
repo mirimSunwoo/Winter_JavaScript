394a984c44e870e118f95bf3e2f8ab0988b08996
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-20 13:41:31 +0900

    기본자료형

---
+++ lecture04.js
@@ -0,0 +1,11 @@
+//자료형이란? 변수에 저장할 수 있는 값의 종류(type)
+//boolean 자료형 : 참, 거짓을 나타내는 자료형
+//ex) number, string, boolean
+//typeof명령어는 어떤 자료형인지 알려준다
+var a = 100, b = 3.14;
+var c = "안녕하세요", d = "a";
+var e = true, f = false;
+
+console.log(a, typeof(a));
+console.log(c, typeof(c));
+console.log(e, typeof(e));
