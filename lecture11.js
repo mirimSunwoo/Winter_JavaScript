62d1b83bf5128f584a8acd8a17a8c04a5427b7f3
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-25 00:43:06 +0900

    관계연산자

---
+++ lecture11.js
@@ -0,0 +1,23 @@
+// Relational 관계
+// Operator 연산자
+
+//<,>,<=,>= 문자도 가능
+//&& and
+//|| or
+//! 반대로
+function solution(a, b){
+    //이 아래줄을 수정해서 a가 b보다 큰지를 return하도록 만들어 보세요.
+    if (a>b){
+        return true
+    }else{
+        return false
+    }
+}
+function solution(a,b){
+    //이 아래줄을 수정해서 a와 b가 모두 True인지를 return하도록 만들어 보세요.
+    if (a&&b == true){
+        return true
+    }else{
+        return false
+    }
+}
