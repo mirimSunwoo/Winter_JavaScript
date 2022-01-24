1e97cf1c1679ab647260b01ecf6007e709f480bf
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-24 23:58:06 +0900

    산술연산자

---
+++ lecture09.js
@@ -0,0 +1,13 @@
+// Arithmetic 산술(산수 계산을 하는)
+// Operator 연산자(계산하라는 기호)
+
+var a = 1;
+console.log(a);
+console.log(a++); //바로 적용되지 않고 1로 찍힌다
+var a = 1;
+console.log(a);
+console.log(++a); //바로 적용되어 2로 찍힌다
+
+Math.pow(2,3); //a의 b승을 구할 수 있음
+Math.sqrt(16); //a의 제곱근을 구함
+Math.random(); //난수
