cd03e492a3cf9333375364aeba7e91f62057cadf
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-27 16:50:07 +0900

    배열 만들기

---
+++ lecture15.js
@@ -0,0 +1,10 @@
+// 배열은 값들의 나열
+// 객체는 값들의 집합
+
+var arr = [];
+var arr2 = [1,2,3,4,5];
+arr2;
+var mixed_arr = [1,true,3.14,"string",{name:"object"},[1,2,3]];
+mixed_arr.length; //=>6
+arr.length;//=>0
+arr2[0]; // 1 범위를 넘어가면 undefined반환
