4ae1baa02fa528777e37ec8b5fc6402823a07293
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-28 14:57:55 +0900

    for문

---
+++ lecture22.js
@@ -0,0 +1,10 @@
+//for문
+
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+for(var i = 0; i<array.length; i++){
+    consolelog(array[i]);
+}
+
+//for문에서 continue문을 만나면 update구문으로 이동
+//while문에서 continue문을 만나면 반복실행 코드의 끝으로 이동한다음 바로 조건식 검사
